8b32a3cb6ec41b730aa0e1c3c8b3fc6bb463b76c
Antonio Góngora <antonio.gongora1491@gmail.com>
2016-11-30 17:06:54 -0600

    Cambio en tamaño de letras y agregamos la traducción a inglés

---
+++ js/traduccion.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#ingles").click(function(){
+    $("#txt1").html("HOME"),
+    $("#txt2").html("US"),
+    $("#txt3").html("EVENTS"),
+    $("#txt4").html("AANY is a family that was formed eight years ago with the aim of identifying great craftsmen / artists and creating an ideal support for the sale and development of their art."),
+    $("#txt5").html("Our mission is to promote the growth of the arts and fine crafts of the Yucatan Peninsula attracting the outstanding talent that is developed here providing a platform for professional, economic and personal growth to the selected artists who are part of our family."),
+    $("#txt6").html("AANY emerged in 2008 on the initiative of Joan Farrell, an American woman who throughout her life has been given the task of finding pieces that stand out for the excellence of its execution. When she lived in the US she was responsible for a gallery of handcrafts. When she arrived to the Yucatan Peninsula, her desire to continue this search for talent led her to found AANY."),
+    $("#txt7").html("Arte a Mano has become an important cultural event in Mérida for residents and visitors. For exhibitors, it has been life changing. In many cases, sales in the exhibition represent a large part of the annual income of our artists."),
+    $("#txt8").html("Volunteers help AANY exhibitors in various areas, from the promotion and presentation of their products, an element that has made a big difference in sales. Artists are encouraged to produce in new and different ways and commercialize more effectively. In addition, it is a space in which you can make interact with the artist and ask custom work. After the event many people visit the artists in their studios and many gallery owners and wholesale stores place orders."),
+    $("#txt9").html("Arte a Mano” is the annual sales platform and promotion for artists belonging to our association. This event is transformed into an idyllic cultural space for selling art, crafts, peninsular artist’s coexistence and a great exhibition of the talent that exists in our country."),
+    $("#txt-1").html("This year our event will take place on 2, 3 and 4 December in the CANACO facilities with the participation of 80 exhibiting artists.")
+  });
+});
